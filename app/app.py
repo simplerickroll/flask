f5297aa5b4a1c80218c6f06eac09589d4e559bd5
KwiatekMiki <79092746+KwiatekMiki@users.noreply.github.com>
2024-04-16 15:18:12 +0200

    c-dev2

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, redirect
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "test"
+
 @app.errorhandler(404)
-def index(_=None):
+def error(_=None):
     return "omg"#redirect("https://www.youtube.com/watch?v=q-Y0bnx6Ndw")
